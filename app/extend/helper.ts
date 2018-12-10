90fe8eb7dff5e2e494a9d981903ef2cee8ed50b3
lzy <luzuoye@qipeipu.com>
2018-12-10 19:04:52 +0800

    feat: 添加bcryptjs定义文件

--- app/extend/helper.ts
+++ app/extend/helper.ts
@@ -1,5 +1,14 @@
-import bcrypt from 'bcryptjs'
+import * as bcrypt from 'bcryptjs'
+import * as moment from 'moment'
 
+moment.locale('zh-cn') // 使用中文
+
+/**
+ * 获取11位随机字符串
+ */
+export function getRandomString() {
+  return Math.random().toString(36).substr(2)
+}
 /**
  * 加密
  * @param str 需要加密的字符串
@@ -15,4 +24,12 @@ export function bhash(str: string) {
  */
 export function bcompare(str: string, hash: string) {
   return bcrypt.compareSync(str, hash)
+}
+
+export function relativeTime(time: number): string {
+  return moment(new Date(time * 1000)).fromNow()
+}
+
+export function domain(url: string): string {
+  return url && url.split('/')[2]
 }
--- typings/app/service/index.d.ts
+++ typings/app/service/index.d.ts
@@ -2,12 +2,10 @@
 // Do not modify this file!!!!!!!!!
 
 import 'egg';
-import ExportNews from '../../../app/service/news';
 import ExportUser from '../../../app/service/user';
 
 declare module 'egg' {
   interface IService {
-    news: ExportNews;
     user: ExportUser;
   }
 }
